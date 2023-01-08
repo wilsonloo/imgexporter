6963de188e15af61919f24c79460f615b94ce4b8
wilson <wilson@miyou.ubuntu>
2023-01-08 21:46:01 +0800

    first commit

---
+++ exportor.lua
@@ -0,0 +1,71 @@
+local tinsert = table.insert
+
+local mt = {}
+mt.__index = mt
+
+--- 绘制线段
+--- line(color, x1, y1, x2, y2 [,...])
+--- line(x1, y1, x2, y2 [, ...])
+function mt:line(x1, y1, x2, y2, ...)
+    local color
+    local points = {...}
+    if #list % 2 == 1 then
+        -- 默认颜色
+        assert(x1, "x1")
+        assert(y1, "y1")
+        assert(x2, "x2")
+        assert(y2, "y2")
+        tinsert(points, 1, y2)        
+        tinsert(points, 1, x2)        
+        tinsert(points, 1, y1)        
+        tinsert(points, 1, x1)        
+    else
+        -- 指定颜色
+        color = x1
+        tinsert(points, 1, y2)        
+        tinsert(points, 1, x2)        
+        tinsert(points, 1, y1)
+    end
+    tinsert(self.lines, {
+        color = color,
+        points = points,
+    })
+end
+
+function mt:rect(x, y, w, h, param)
+    local color
+    if param then
+        color = x
+        x = y
+        y = w
+        w = h
+        h = param
+    end
+    tinsert(self.rects, {
+        color = color,
+        x = x,
+        y = y,
+        w = w,
+        h = h,
+    })
+end
+
+function mt:write(filename)
+    
+end
+
+local M = {}
+
+function M.new(width, heigh, title)
+    local obj = {
+        width = width,
+        heigh = heigh,
+        title = title,
+
+        lines = {},
+        rects = {},
+    }
+    return setmetatable(obj, mt)
+end
+
+return M
